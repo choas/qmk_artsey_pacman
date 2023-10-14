1a02191007218d10f9c995ba3b1e3d2fa197d79d
Lars Gregori <138015+choas@users.noreply.github.com>
2023-10-14 18:18:04 +0200

    right hand flag

--- pacman.h
+++ pacman.h
@@ -6,26 +6,26 @@
 #include "quantum.h"
 #include "shared_defines.h"
 
-#define LEFT_HAND
+#ifdef HAND_RIGHT
 
-#ifdef LEFT_HAND
-
-#define LAYOUT_4x2( \
-	K000, K001, K002, K003, \
-    K004, K005, K006, K007 \
-) { \
-	{ K000,  K001,  K002,  K003}, \
-    { K004,  K005,  K006,  K007 } \
-}
+    // right hand
+    #define LAYOUT_4x2( \
+        K003, K002, K001, K000, \
+        K007, K006, K005, K004 \
+    ) { \
+        { K000,  K001,  K002,  K003}, \
+        { K004,  K005,  K006,  K007 } \
+    }
 
 #else
 
-#define LAYOUT_4x2( \
-	K003, K002, K001, K000, \
-    K007, K006, K005, K004 \
-) { \
-	{ K000,  K001,  K002,  K003}, \
-    { K004,  K005,  K006,  K007 } \
-}
+    // left hand
+    #define LAYOUT_4x2( \
+        K000, K001, K002, K003, \
+        K004, K005, K006, K007 \
+    ) { \
+        { K000,  K001,  K002,  K003}, \
+        { K004,  K005,  K006,  K007 } \
+    }
 
 #endif
--- rules.mk
+++ rules.mk
@@ -5,4 +5,10 @@ COMBO_ENABLE = yes
 
 RAW_ENABLE = yes
 
+ifeq ($(strip $(HAND)), right)
+	HAND_RIGHT = yes
+	OPT_DEFS += -DHAND_RIGHT
+endif
+
+
 # CONSOLE_ENABLE = yes
